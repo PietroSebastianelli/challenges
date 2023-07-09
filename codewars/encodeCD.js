2a3701100d8782fab0af531511f62e1981ec9a3a
pietrosebastianelli <137496450+PietroSebastianelli@users.noreply.github.com>
2023-07-09 09:39:13 +0200

    encodeCD.js

---
+++ codewars/encodeCD.js
@@ -0,0 +1,17 @@
+function encodeCD(n){
+    let bin = n.toString(2);
+    bin = bin.padStart(8,'0');
+    let pitlands = ['P'];
+    for (let i = 0; i < 8; i++){
+        if (bin[8-i-1]=='1'){
+            if (pitlands[pitlands.length-1] == 'P') pitlands.push('L');
+            else pitlands.push('P');
+
+        }
+        else pitlands.push(pitlands[pitlands.length-1]);
+    }
+
+
+    return pitlands.join('');
+}
+console.log(encodeCD(5));
