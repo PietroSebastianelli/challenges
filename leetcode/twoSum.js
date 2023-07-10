e57f5a56550c49f39b7e9dd3bcd94b9c1537f485
pietrosebastianelli <137496450+PietroSebastianelli@users.noreply.github.com>
2023-07-10 17:05:28 +0200

    .

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -5,13 +5,14 @@
  */
 // The brute force solution is trivial, the important point is to find a way to do it better than O(n**2)
 // We do this by first applying mergesort algo to the array, then we find the solution by divide and conquer apllied to the sorted array
-// it seems pointless to rewrite the mergesort for the exercise, but we can suppose that the sorting function has time complexity of O(n*log n)
+// in order to return the precise index we need to rewrite mergesort in order to remember the array
+// for now i will quickly write the brute force solution:
 
 var twoSum = function(nums, target) {
-    nums.sort();
-    return sortedTwoSum(nums);
-    
-    
+    let l = nums.length;
+    for (let i = 0; i < l; i++){
+        for(let j=i+1;j<l;j++){
+            if (nums[i]+nums[j] == target) return [i,j];
+        }
+    }    
 };
-
-function sortedTwoSum(nums, target)
